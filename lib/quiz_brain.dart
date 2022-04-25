6e04fe9bcdc152d96640d4d1edab674b78f82b53
chibuezepaul <chibuezepaul2@gmail.com>
2022-04-26 00:57:31 +0100

    quizz app

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,47 @@
+import 'question.dart';
+
+class QuizzBrain {
+  int _questionPointer = 0;
+  List<Question> _questions = [
+    Question("You can lead a cow downstairs but not upstairs", false),
+    Question("Approximately one quarter of human bones are in the feet", true),
+    Question("A slug's blood is green", true),
+    Question('Some cats are actually allergic to humans', true),
+    Question("Buzz Aldrin's mother's maiden name was 'Moon'.", true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question("Google was originally called 'Backrub'.", true),
+    Question(
+        "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.",
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  String getQuestionText() => _questions[_questionPointer].questionText;
+
+  bool isAnswerCorrect() =>
+      _questions[_questionPointer].questionAnswer == true ? true : false;
+
+  void nextQuestion() {
+    if (_questionPointer < _questions.length - 1) {
+      _questionPointer++;
+    }
+  }
+
+  bool isFinished() => _questionPointer == _questions.length - 1;
+
+  void reset() => _questionPointer = 0;
+}
